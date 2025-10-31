7fde05df4c525b70405d37b7d24412fe56971870
fescobar116 <docente.fescobar@unac.edu.co>
2025-10-31 16:33:58 -0500

    testing

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,4 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testMultiplication() {
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.multiply(2, 3));
+    }
 }
